e9bc8289d8d65e9487d693f88470dc78e62d9861
JmlKhan <jmlkhon17@gmail.com>
2021-12-28 17:39:02 +0500

    add db context

---
+++ TodoAPP/Model/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAPP.Model
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; } = false;
+    }
+}
--- TodoAPP/Program.cs
+++ TodoAPP/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAPP.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+{
+    opt.UseSqlServer(connectionString);
+})
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
